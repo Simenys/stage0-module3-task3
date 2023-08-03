18692750ae2d75d69893b6288ea850fedc53d719
Simonas Zilevicius <s.zilevicius@gmail.com>
2023-08-03 17:26:37 +0300

    Descriptions are sometimes tricky

--- src/main/java/school/mjc/stage0/module3/task3/ReassigningValues.java
+++ src/main/java/school/mjc/stage0/module3/task3/ReassigningValues.java
@@ -3,8 +3,8 @@ package school.mjc.stage0.module3.task3;
 public class ReassigningValues {
     public static void main(String[] args) {
         int first = 1;
-        int second = 100;
-        int third = 1000;
+        int second = 10;
+        int third = 100;
         System.out.println(first);
         System.out.println(second);
         System.out.println(third);
@@ -15,6 +15,9 @@ public class ReassigningValues {
         first = 15;
         second = 6;
         third = 4;
+        System.out.println(first);
+        System.out.println(second);
+        System.out.println(third);
         System.out.println(linkToFirst);
         System.out.println(linkToSecond);
         System.out.println(linkToThird);
